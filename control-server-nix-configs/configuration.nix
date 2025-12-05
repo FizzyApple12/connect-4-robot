c6585c8f9d9cd1ce7728835d68875ac3bdf1fc47
FizzyApple12 <github@fizzyapple12.com>
2025-12-05 16:12:41 -0500

    add nixos configurations

---
+++ control-server-nix-configs/configuration.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "connect4server";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Indiana/Indianapolis";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  services.xserver.xkb.layout = "us";
+
+  users.users.fizzyapple12 = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+  ];
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "25.05";
+}
+
---
+++ control-server-nix-configs/hardware-configuration.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/cf96602e-1a55-45cb-95b9-09c4349ff7eb";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/268D-74F7";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/413b3029-2fb4-4a9e-a505-9e23784e4c33"; }
+    ];
+
+  networking.useDHCP = lib.mkDefault true;
+  
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
